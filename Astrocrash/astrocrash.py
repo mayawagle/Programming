4eaf8260005110344d0e356007e9ddbd49e366fa
mayawagle <106615917+mayawagle@users.noreply.github.com>
2022-06-09 14:31:28 -0700

    Astrocrash 04
    added missile class

--- Astrocrash/astrocrash.py
+++ Astrocrash/astrocrash.py
@@ -11,14 +11,16 @@ class Asteroid(games.Sprite):
     SMALL = 1
     MEDIUM = 2
     LARGE = 3
-    images = {SMALL : games.load_image("asteroid_small.bmp"), MEDIUM : games.load_image("asteroid_med.bmp"), LARGE : games.load_image("asteroid_big.bmp") }
-
     SPEED = 2
+    images = {SMALL : games.load_image("asteroid_small.bmp"), MEDIUM : games.load_image("asteroid_med.bmp"), LARGE : games.load_image("asteroid_big.bmp") }
 
     def __init__(self, x, y, size):
         #initialize asteroid sprite
-        super(Asteroid,self).__init__(image = Asteroid.images[size], x = x, y = y, dx = random.choice([1, -1]) * Asteroid.SPEED * random.random()/size, dy = random.choice([1, -1]) * Asteroid.SPEED * random.random()/size)
-
+        super(Asteroid,self).__init__(
+            image = Asteroid.images[size],
+            x = x, y = y, 
+            dx = random.choice([1, -1]) * Asteroid.SPEED * random.random()/size, 
+            dy = random.choice([1, -1]) * Asteroid.SPEED * random.random()/size)
         self.size = size
 
     def update(self):
@@ -49,13 +51,18 @@ class Ship(games.Sprite):
         if games.keyboard.is_pressed(games.K_RIGHT):
          self.angle += Ship.ROTATION_STEP
 
-        #apple thrust based on up arrow key
+        #apply thrust based on up arrow key
         if games.keyboard.is_pressed(games.K_UP):
             Ship.sound.play()
             #change velocity components based on ship's angle
             angle = self.angle * math.pi / 180 #convert to radians
             self.dx += Ship.VELOCITY_STEP * math.sin(angle)
             self.dy += Ship.VELOCITY_STEP * -math.cos(angle)
+
+        #fire missile if spacebar pressed
+        if games.keyboard.is_pressed(games.K_SPACE):
+            new_missile = Missile(self.x, self.y, self.angle)
+            games.screen.add(new_missile)
 
         #wrap the ship around the screen
         if self.top > games.screen.height:
@@ -66,7 +73,58 @@ class Ship(games.Sprite):
 
         if self.left > games.screen.width:
             self.right = 0
-            
+
+        if self.right < 0:
+            self.left = games.screen.width
+    
+class Missile(games.Sprite):
+     #a missile launched by the player's ship
+    image = games.load_image("missile.bmp")
+    sound = games.load_sound("missile.wav")
+    BUFFER = 40
+    VELOCITY_FACTOR = 7
+    LIFETIME = 40
+
+    def __init__(self, ship_x, ship_y, ship_angle):
+        #initialize missile sprite
+         Missile.sound.play()
+
+         #convert to radians
+         angle = ship_angle * math.pi / 180
+
+         #calculate the missile's starting position
+         buffer_x = Missile.BUFFER * math.sin(angle)
+         buffer_y = Missile.BUFFER * -math.cos(angle)
+         x = ship_x + buffer_x
+         y = ship_y + buffer_y
+         
+         #calculate the missile's velocity components
+         dx = Missile.VELOCITY_FACTOR * math.sin(angle)
+         dy = Missile.VELOCITY_FACTOR * -math.cos(angle)
+
+         #create the missile
+         super(Missile, self).__init__(image = Missile.image, x = x,
+         y = y, dx = dx, dy = dy)
+        
+         self.lifetime = Missile.LIFETIME
+    
+    def update(self):
+        #move the missile 
+        #if lifetime is up, destroy the missile
+        self.lifetime -= 1
+        if self.lifetime == 0:
+            self.destroy()
+        
+        #wrap the missile around screen
+        if self.top > games.screen.height:
+            self.bottom = 0
+
+        if self.bottom < 0:
+            self.top = games.screen.height
+
+        if self.left > games.screen.width:
+            self.right = 0
+
         if self.right < 0:
             self.left = games.screen.width
 
